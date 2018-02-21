00c23c5957007e73a6b442169736937a8910f987
PSLCOL\vjimenezt <vjimenezt@psl.com.co>
2018-02-21 15:01:15 -0500

    Improving Test

--- protractor.config.js
+++ protractor.config.js
@@ -1,5 +1,8 @@
 ﻿exports.config = {
   framework: 'jasmine',
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['test/spec.js']
+  specs: ['test/Google.spec.js'],
+  onPrepare: () => {
+    browser.ignoreSynchronization = true;
+  }
 }
