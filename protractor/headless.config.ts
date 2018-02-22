ae926ed471f5977570f245b7f199471a50155b7b
victorhugojt <36706669+victorhugojt@users.noreply.github.com>
2018-02-22 10:33:25 -0500

    headless test (#5)

---
+++ protractor/headless.config.ts
@@ -0,0 +1,19 @@
+﻿import { browser, Config } from 'protractor';
+import { reporter }   from './helpers/reporter';
+
+export const config: Config = {
+  framework: 'jasmine',
+  SELENIUM_PROMISE_MANAGER: false,
+  specs: ['../test/Google.spec.js'],
+  noGlobals: true,
+  capabilities: {
+    browserName: 'chrome',
+    chromeOptions: {
+      args: ['--headless', '--disable-gpu', '--window-size=800,600']
+    }
+  },
+  onPrepare: () => {
+    browser.ignoreSynchronization = true;
+    reporter();
+  }
+}
