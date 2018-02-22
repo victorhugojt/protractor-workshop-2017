b90a5b29b9a3f89ba16fc22aae5bbf96b97d4ea7
PSLCOL\vjimenezt <vjimenezt@psl.com.co>
2018-02-22 07:26:39 -0500

    No selenium

--- protractor/config.ts
+++ protractor/config.ts
@@ -3,7 +3,7 @@ import { reporter }   from './helpers/reporter';
 
 export const config: Config = {
   framework: 'jasmine',
-  seleniumAddress: 'http://localhost:4444/wd/hub',
+  SELENIUM_PROMISE_MANAGER: false,
   specs: ['../test/Google.spec.js'],
   noGlobals: true,
   onPrepare: () => {
