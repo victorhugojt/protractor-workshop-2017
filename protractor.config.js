1356cb6443fe526148ef14db5fe4b3c0c33be829
victorhugojt <36706669+victorhugojt@users.noreply.github.com>
2018-02-22 07:09:43 -0500

    Migrate to typescript (#3)
    * Migrating to TypeScript
    
    * TypeScript configuration
    
    * Fix script tags
    
    * PR recomendations

--- protractor.config.js
+++
@@ -1,8 +0,0 @@
-﻿exports.config = {
-  framework: 'jasmine',
-  seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['test/Google.spec.js'],
-  onPrepare: () => {
-    browser.ignoreSynchronization = true;
-  }
-}
---
+++ protractor/config.ts
@@ -0,0 +1,13 @@
+﻿import { browser, Config } from 'protractor';
+import { reporter }   from './helpers/reporter';
+
+export const config: Config = {
+  framework: 'jasmine',
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['../test/Google.spec.js'],
+  noGlobals: true,
+  onPrepare: () => {
+    browser.ignoreSynchronization = true;
+    reporter();
+  }
+}
