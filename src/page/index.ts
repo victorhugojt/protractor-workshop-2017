5cfd30901954f1ff711c388f97c8f38edfab815a
PSLCOL\vjimenezt <vjimenezt@psl.com.co>
2018-02-23 09:44:37 -0500

    clean code

--- src/page/index.ts
+++ src/page/index.ts
@@ -1,3 +1,4 @@
 export { MenuContentPage } from './MenuContent.page';
 export { AddProductModalPage } from './ProductAddedModal.page';
-export { CartButtonPage } from './ProductAddedModal.page';
+export { CartButtonPage } from './CartButtonPage.page';
+
